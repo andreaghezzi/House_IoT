90732a8872ab9e0f44ae8af09ecc8f63881cbb96
AlessandroGhezzi <alessandro@MacBook-Pro-di-Alessandro.local>
2017-03-25 11:11:01 +0100

    Update the api
    Now api can return value, status and all info of a device

--- api/api/controllers/device.js
+++ api/api/controllers/device.js
@@ -1,27 +1,51 @@
 'use strict';
 
 var util = require('util');
+var app = require('../../app');
 var connection;
 
 module.exports = {
 		getStatus: getStatus,
-		getValue: getValue
+		getValue: getValue,
+		getInfo: getInfo
 };
 
 function getStatus(req, res) {
 	var deviceName = req.swagger.params.deviceName.value || 'unknow';
 	
 	dbConnection();
-	connection.query('SELECT status AS solution FROM device WHERE name="'+deviceName+'"', function (error, results, fields) {
-	  if (error) throw error;
-	  res.json(results[0].solution);
+	connection.query('SELECT status FROM device WHERE name="'+deviceName+'"', function (error, results, fields) {
+	  if (error)
+		  throw error;
+	  res.json(results[0]); //Return the device status in json format
 	});
 	connection.end();
 }
 
 function getValue(req, res) {
 	var deviceName = req.swagger.params.deviceName.value || 'unknow';
-	res.json(deviceName);
+	
+	dbConnection();
+	connection.query('SELECT value FROM device WHERE name="'+deviceName+'"', function (error, results, fields) {
+	  if (error)
+		  throw error;
+	  res.json(results[0]); //Return the device value in json format
+	});
+	connection.end();
+}
+
+function getInfo(req, res) {
+	var deviceName = req.swagger.params.deviceName.value || 'unknow';
+	
+	dbConnection();
+	connection.query('SELECT * FROM device WHERE name="'+deviceName+'"', function (error, results, fields) {
+	  if (error)
+		  throw error;
+	  if(results.length > 0) {          
+        res.json(results[0]); //Return the device info in json format
+      }
+	});
+	connection.end();
 }
 
 function dbConnection() {
